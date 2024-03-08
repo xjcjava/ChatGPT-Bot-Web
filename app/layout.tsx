1333a94a87f3586b74aeb49a210718b95e254dab
xjcjava <47705622+xjcjava@users.noreply.github.com>
2024-03-08 14:35:56 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Bot Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "和谐AI",
+  description: "与AI共生.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -19,7 +19,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Bot Web",
+    title: "和谐AI",
     statusBarStyle: "default",
   },
 };
